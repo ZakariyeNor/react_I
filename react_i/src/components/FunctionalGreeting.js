ede4ab998a3b185186ce8a329f0de8b282d22648
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-07-24 15:36:09 +0000

    Create stateless class component

---
+++ react_i/src/components/FunctionalGreeting.js
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "../App";
+
+
+/* function FunctionalGreeting() {
+    return <h1>Hello from React</h1>
+} */
+
+const FunctionalGreeting = () => <h1>Hello from React</h1>;
+export default FunctionalGreeting;
