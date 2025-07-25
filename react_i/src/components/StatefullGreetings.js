758692a4814743b3f5db418c50d51721aa8ec20a
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-07-25 08:47:55 +0000

    Set callback function

--- react_i/src/components/StatefullGreetings.js
+++ react_i/src/components/StatefullGreetings.js
@@ -9,11 +9,22 @@ class StatefullGreetings extends React.Component {
             buttonText: "Add",
         };
     }
+    handleClick() {
+        this.setState({
+            introduction: "Goodbye!",
+            buttonText: "Exit",
+        }, ()=> {
+            console.log("New header value:", this.state.introduction)
+            console.log("New button value:", this.state.buttonText)
+        });
+        console.log(this.state.introduction)
+        console.log(this.state.buttonText)
+    }
     render() {
         return (
             <div>
                 <h1>{this.state.introduction} {this.props.name} {this.props.greeting} {this.props.option}</h1>
-                <button>{this.state.buttonText}</button>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
             </div>
         )
     }
